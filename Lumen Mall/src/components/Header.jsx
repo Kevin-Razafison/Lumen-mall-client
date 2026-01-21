d9d9f6e74d1c062865906171a7bba89ccef6daa5
kevin <angelarknarem@gmail.com>
2026-01-21 09:46:49 +0300

    feat: implement logo component with brand styling

--- Lumen Mall/src/components/Header.jsx
+++ Lumen Mall/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Logo from './Logo'
 import styles from './Header.module.css'
 
 const Header = () => {
     return(
         <header className={styles.headerContainer}>
+            <Logo />
         </header>   
     )
 }
---
+++ Lumen Mall/src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './Logo.module.css'
+
+const Logo = () => {
+    return (
+        <div className={styles.LogoContainer}>
+            <img src="../public/Lumen-Mall-logo.png" alt="logo" className={styles.LogoImg} />
+        </div>
+    )
+}
+
+export default Logo

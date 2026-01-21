99bac603872c1b2995e1f4c01840c31c2243c856
kevin <angelarknarem@gmail.com>
2026-01-21 20:23:14 +0300

    feat: link header login button to login page

--- Lumen Mall/src/components/header/UserAccount.jsx
+++ Lumen Mall/src/components/header/UserAccount.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from './UserAccount.module.css'
 
 const UserAccount = ({ userName = "Login/Sign up"}) => {
     return (
-        <div className={styles.container}>
+        <Link to="/login" className={styles.container}>
             <div className={styles.iconSection}>
                 <img src="/icons/icons-user.png" alt="user-icon" />
             </div>
@@ -11,7 +12,7 @@ const UserAccount = ({ userName = "Login/Sign up"}) => {
                 <span className={styles.greeting}>Welcome</span>
                 <span className={styles.actionText}>{userName}</span>
             </div>
-        </div>
+        </Link>
     )
 }
 export default UserAccount

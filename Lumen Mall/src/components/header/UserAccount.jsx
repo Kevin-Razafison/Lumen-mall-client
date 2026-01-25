a257a6767a7c669d3ee8a53682e11f539ba0eee2
Kevin-Razafison <angelarknarem@gmail.com>
2026-01-25 13:31:35 +0300

    feat: implement the header useraccountupdate

--- Lumen Mall/src/components/header/UserAccount.jsx
+++ Lumen Mall/src/components/header/UserAccount.jsx
@@ -7,8 +7,8 @@ const UserAccount = () => {
     const { user, logout, isAuthenticated } = useAuth();
 
     return (
-        <div className={styles.container}>
-            <Link to={isAuthenticated ? "/profile" : "/login"} className={styles.linkWrapper}>
+        <div className={styles.mainWrapper}>
+            <Link to={isAuthenticated ? "/profile" : "/login"} className={styles.container}>
                 <div className={styles.iconSection}>
                     <img src="/icons/icons-user.png" alt="user-icon" />
                 </div>
@@ -22,7 +22,6 @@ const UserAccount = () => {
                 </div>
             </Link>
             
-            {/* Optional: Add a logout button that appears only when logged in */}
             {isAuthenticated && (
                 <button onClick={logout} className={styles.logoutBtn}>Sign Out</button>
             )}

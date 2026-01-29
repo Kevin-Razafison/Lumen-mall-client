e3ce3d5a9246ea0fac90b6206030fb8668e57ee3
Kevin-Razafison <angelarknarem@gmail.com>
2026-01-29 10:53:49 +0300

    fix: fix userLocation detection

--- Lumen Mall/src/components/header/Header.jsx
+++ Lumen Mall/src/components/header/Header.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import DesktopHeader from './DesktopHeader'
 import MobileHeader from './mobile/MobileHeader';
-import { useLocation } from '../../context/LocationContext';
+import { useUserLocation } from '../../context/LocationContext';
 
 const Header = ({ openLocationModal }) => {
-  const { location } = useLocation();
+  const { location } = useUserLocation();
   const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
 
   useEffect(() => {

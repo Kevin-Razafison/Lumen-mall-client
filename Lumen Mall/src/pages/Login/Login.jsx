aaf0f62690f15cdb9cab8b2475e45bee1223c656
Kevin-Razafison <angelarknarem@gmail.com>
2026-01-31 00:14:14 +0300

    fix: fix the handlesubmit2

--- Lumen Mall/src/pages/Login/Login.jsx
+++ Lumen Mall/src/pages/Login/Login.jsx
@@ -57,7 +57,7 @@ const Login = () => {
     }
   };
   
-const handleSubmit = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     setError('');
     setLoading(true);
